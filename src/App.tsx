936744987c4f0342f06a0eee195c23fbb16bad27
bagoesrex <bagoesrex@gmail.com>
2025-06-05 16:23:12 +0700

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="logo-container">
+        <img src="/vite.svg" className="logo" alt="Logo" />
+      </div>
+      <h1>🚧 Under Maintenance</h1>
+      <p className="message">
+        This page is currently under development. Please check back later.
+      </p>
+    </div>
+  )
+}
+
+export default App
