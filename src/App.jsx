0b7db266de905de68e8c4a19d9c817da242f60eb
bagoesrex <bagoesrex@gmail.com>
2025-06-23 08:16:33 +0700

    feat: create TerminalBox component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
 import BackgroundImage from './components/BackgroundImage'
+import TerminalBox from './components/TerminalBox'
 
 function App() {
   return (
     <>
+      <div className="h-screen w-full flex justify-center items-center">
+        <div className="w-full">
+          <TerminalBox />
+        </div>
+      </div>
       <BackgroundImage />
     </>
   )
---
+++ src/components/TerminalBox.jsx
@@ -0,0 +1,19 @@
+export default function TerminalBox() {
+    return (
+        <>
+            <div className="max-w-4xl h-[55vh] mx-auto p-4 text-xl text-gray-300 overflow-y-auto 
+            border-2 border-slate-200 
+            rounded-md bg-black/35">
+                <h1>Test</h1>
+                <h1>Test</h1>
+                <h1>Test</h1>
+                <h1>Test</h1>
+                <h1>Test</h1>
+                <h1>Test</h1>
+                <h1>Test</h1>
+                <h1>Test</h1>
+                <h1>Test</h1>
+            </div>
+        </>
+    )
+}
