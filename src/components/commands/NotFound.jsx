c91dd8646b4069044723c5bf847b22019f66aa75
bagoesrex <bagoesrex@gmail.com>
2025-06-24 13:04:54 +0700

    feat: add NotFound component to handle unknown commands

--- src/components/commands/NotFound.jsx
+++ src/components/commands/NotFound.jsx
@@ -1,7 +1,25 @@
+import Animate from "react-smooth";
+
 export default function NotFound() {
     return (
-        <>
-            <h1>it's NotFound</h1>
-        </>
-    )
+        <div className="">
+            <Animate
+                from={{
+                    opacity: 0,
+                    transform: "translateY(10px)",
+                }}
+                to={{
+                    opacity: 1,
+                    transform: "translateY(0px)",
+                }}
+                duration={400}
+                easing="ease-in-out"
+            >
+                <div className="flex flex-col items-center">
+                    <h1 className="text-red-400 my-0.5 w-35">Command not found</h1>
+                    <h1 className="my-0.5 w-35">Try: ls</h1>
+                </div>
+            </Animate>
+        </div>
+    );
 }
